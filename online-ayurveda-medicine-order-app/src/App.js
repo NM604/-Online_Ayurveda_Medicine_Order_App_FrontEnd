91a0f86b936d14d878690a56286ea78283554ce1
Nikhil <nikhilmath99@gmail.com>
2022-11-07 16:52:31 +0530

    Adds files for admin and customer login

--- online-ayurveda-medicine-order-app/src/App.js
+++ online-ayurveda-medicine-order-app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AdminLogin from "./components/Admin/AdminLogin";
+import CustomerLogin from "./components/Customer/CustomerLogin";
+import Admin from "./components/Admin/Admin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/customer/login" element={<CustomerLogin />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
